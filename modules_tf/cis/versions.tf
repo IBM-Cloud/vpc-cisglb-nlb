44d75cac05864513a40497ba4281b5e5e4e2e50f
Powell Quiring <powellquiring@gmail.com>
2022-12-04 18:15:55 -0800

    polish

--- modules_tf/cis/versions.tf
+++ modules_tf/cis/versions.tf
@@ -4,8 +4,8 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "> 1.48.0"
+      version = ">= 1.48.0"
     }
   }
-  required_version = ">= 1.3.6"
+  required_version = ">= 1.3.2"
 }
